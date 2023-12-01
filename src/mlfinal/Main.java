89396d4722b01a819c7d714e8efd091b30db608c
John Hamre <jjha2020@mymail.pomona.edu>
2023-11-30 21:22:42 -0800

    Probably Breaks Something
    Blame the gitignore not cloning, Dee Colon

--- src/mlfinal/Main.java
+++ src/mlfinal/Main.java
@@ -1,13 +1,30 @@
 package mlfinal;
 
+import javapoker.Card;
+import javapoker.Deck;
+
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello Stanley");
 	}
 	
 	public static Example generateExample() {
+		// The example to be returned. Created empty.
 		Example outputExample = new Example();
+		// Create a new deck.
+		Deck deck = new Deck();
+		// Create the hands to draw into
+		Card[] playerHand = new Card[2];
+		Card[] board = new Card[5];
+		Card[] opponentHand = new Card[2];
+		for(int i = 0; i <= 1; i++) playerHand[i] = deck.drawFromDeck();
+		for(int i = 0; i <= 4; i++) board[i] = deck.drawFromDeck();
+		for(int i = 0; i <= 1; i++) opponentHand[i] = deck.drawFromDeck();
 		
+		// Generate the label from hand logic.
+		
+		double label = 0.0;
+		// Set values
 		
 		return outputExample;
 	}
